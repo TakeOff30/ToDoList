83b595df27c4adbbbded5b667039274578d61b40
Oliver <oliver20669@gmail.com>
2022-08-23 18:01:58 +0200

    Html content setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,4 @@ module.exports = {
     entry: {
         main: path.resolve(__dirname, 'src/index.js'),
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-            filename: 'index.html',
-        }),
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        clean: true,
-        assetModuleFilename: '[name][ext]',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
 };
